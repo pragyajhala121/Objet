1bf20ae152d189f8053d6be175b5f31981591e34
pragyajhala121 <73744592+pragyajhala121@users.noreply.github.com>
2021-04-21 18:32:06 +0530

    Add files via upload

---
+++ Cup.js
@@ -0,0 +1,46 @@
+objectDetector= "";
+
+img = "";
+objects = [];
+status = "";
+
+function preload(){
+    img = loadImage("Cup.jpg");
+}
+function setup(){
+    canvas = createCanvas(640,540);
+    canvas.center();
+}
+
+function draw(){
+ 
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    if (status != undefined) {
+  	  image(img, 0, 0, 640, 540);
+    for (var i = 0; i < objects.length; i++) {
+      document.getElementById("status").innerHTML = "Status : Objects Detected";
+
+      fill(255, 0, 0);
+      percent = floor(objects[i].confidence * 100);
+      text(objects[i].label + " " + percent + "%", objects[i].x + 5, objects[i].y + 15);
+      noFill();
+      stroke(255, 0, 0);
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+  }
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        objects= results;
+    }
+}
